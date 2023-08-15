09409a2fe74ab4b24e2e10bf6508cdea2f4ee706
paulkah <paulkwao23@yahoo.com>
2023-08-15 03:09:57 -0900

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#!/bin/bash
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - point Entry 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        puts(“\"Programming is like building a multilingual puzzle”)
+        puts(“\"Programming is like building a multilingual puzzle”);
         return (0);
 }
 
